3de583697ab28c5cfaef3f24776de7e4e9f94740
Tim Wu <timothywu96@gmail.com>
2020-10-10 00:39:57 +0800

    refactor

--- face_rec_stream.py
+++ face_rec_stream.py
@@ -5,10 +5,11 @@ import numpy as np
 import imutils
 import pandas as pd
 
-df = pd.read_pickle(r'/Users/timwu/custom_face_data/custom_embeddings')
+df = pd.read_pickle('custom_embeddings.pkl')
 
 names = df.Name.tolist()
 encodings = df.Encodings.tolist()
+
 
 def get_boxes_encodings(img_path):
     img = cv2.cvtColor(cv2.imread(img_path), cv2.COLOR_BGR2RGB)
@@ -22,7 +23,7 @@ def most_common(array):
     return max(set(array), key=array.count)
 
 
-def KNN(frame_enc, encodings, names, neighbors=5):
+def knn(frame_enc, encodings, names, neighbors=5):
     dist_list = np.array([np.linalg.norm(frame_enc - i) for i in encodings])
     idx = np.argsort(dist_list)[:neighbors]
     names = np.array(names)[idx]
@@ -43,7 +44,7 @@ while True:
         top, right, bottom, left = box
         cv2.rectangle(frame, (left, top), (right, bottom), color=(255, 0, 0), thickness=2)
         y = top - 15 if top - 15 > 15 else top + 15
-        cv2.putText(frame, KNN(enc, encodings, names), (int((right + left) / 2), y),
+        cv2.putText(frame, knn(enc, encodings, names), (int((right + left) / 2), y),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 255), 2)
     cv2.imshow("Stream", frame)
 
